4c32f979a86873a61946d41900d73f1934b499b3
project <berkayyozayy@gmail.com>
2020-03-05 21:26:46 +0300

    first

---
+++ app.js
@@ -0,0 +1,34 @@
+eventListeners();
+
+
+function eventListeners(){
+
+    document.getElementById("translate-form").addEventListener("submit",translateWord);
+
+    //Arayuz Islemleri
+    document.getElementById("language").onchange = function(){
+
+        ui.changeUI()
+    }
+
+}
+
+const translate = new Translate(document.getElementById("word").value, document.getElementById("language").value);
+
+const ui = new UI();
+
+function translateWord(e){
+
+
+    translate.changeParameters(document.getElementById("word").value,document.getElementById("language").value);
+
+    translate.translateWord(function(err, response){
+        if(err){
+            console.log(err); //Hata 
+        }else{
+            ui.displayTranslate(response);
+        }
+    });
+
+    e.preventDefault();
+}
